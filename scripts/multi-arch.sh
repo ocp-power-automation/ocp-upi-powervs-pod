268d90079a27fd88e9eb566d8b7f472e3aea5bde
Rafael Peria de Sene <rpsene@br.ibm.com>
2021-01-13 11:02:35 -0300

    Update Travis build to push the multiarch images to a new Quay.io repo
    Signed-off-by: Rafael Peria de Sene <rpsene@br.ibm.com>

--- scripts/multi-arch.sh
+++ scripts/multi-arch.sh
@@ -11,9 +11,9 @@ OCP_VERSION=ocp-$1
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-docker manifest create quay.io/powercloud/ocp4-powervs-automation-runtime:$OCP_VERSION \
-quay.io/powercloud/ocp4-powervs-automation-runtime:ocp-$VERSION-x86_64 quay.io/powercloud/ocp4-powervs-automation-runtime:ocp-$VERSION-ppc64le
+docker manifest create quay.io/powercloud/ocp-upi-powervs-pod:$OCP_VERSION \
+quay.io/powercloud/ocp-upi-powervs-pod:ocp-$VERSION-x86_64 quay.io/powercloud/ocp-upi-powervs-pod:ocp-$VERSION-ppc64le
 
 docker login quay.io -u $USER_QUAY -p $PWD_QUAY
 
-docker manifest push quay.io/powercloud/ocp4-powervs-automation-runtime:$OCP_VERSION
+docker manifest push quay.io/powercloud/ocp-upi-powervs-pod:$OCP_VERSION
